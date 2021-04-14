cd8316533fbe6e1ae28b8b18902e69308897ae16
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-04-14 15:43:23 -0300

    refactor: pass loggedIn property to component

--- stories/otw-app.stories.js
+++ stories/otw-app.stories.js
@@ -9,11 +9,12 @@ export default {
   },
 };
 
-function Template({ title, backgroundColor }) {
+function Template({ title, backgroundColor, loggedIn }) {
   return html`
     <otw-app
       style="--otw-app-background-color: ${backgroundColor || 'white'}"
       .title=${title}
+      ._loggedIn=${loggedIn}
     >
     </otw-app>
   `;
@@ -22,4 +23,5 @@ function Template({ title, backgroundColor }) {
 export const App = Template.bind({});
 App.args = {
   title: 'My app',
+  loggedIn: false,
 };
