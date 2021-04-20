464bbda0c6b363ee6245d1ae12d2fa69fd530fe1
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2021-04-20 08:14:42 -0300

    refactor: rename firebaseAppConfig file

---
+++ config/firebaseAppConfig_example.js
@@ -0,0 +1,10 @@
+const firebaseAppConfig = {
+  apiKey: 'project-api-key',
+  authDomain: 'project-name.firebaseapp.com',
+  databaseURL: 'https://project-default-db.firebaseio.com',
+  projectId: 'project_id',
+  storageBucket: 'project_id.appspot.com',
+  messagingSenderId: 'xxxxxxxx',
+  appId: 'very-long-app-id-hex',
+};
+module.exports = firebaseAppConfig;
